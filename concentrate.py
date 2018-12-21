efe9ca8555318de20aceb3863f15d74f4e8abe7d
Mark Bolding <mark.bolding@gmail.com>
2018-12-21 17:48:38 -0600

    calculator and timer

---
+++ concentrate.py
@@ -0,0 +1,17 @@
+import time
+import datetime
+
+start_time = time.time()
+
+
+try:
+    while True:
+        time.sleep(1)
+        elapsed_time = time.time() - start_time
+        print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)),end='\r',flush=True)
+except KeyboardInterrupt:
+    print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+
+
+
+# your script
---
+++ tkconcentrate.py
@@ -0,0 +1,22 @@
+# for python 3.x use 'tkinter' rather than 'Tkinter'
+import tkinter as tk
+import time
+
+
+
+class App():
+    def __init__(self):
+        self.start_time = time.time()
+        self.root = tk.Tk()
+        self.label = tk.Label(text="")
+        self.label.pack()
+        self.update_clock()
+        self.root.mainloop()
+
+    def update_clock(self):
+        self.elapsed_time = time.time() - self.start_time
+        now = time.strftime("%H:%M:%S", time.gmtime(self.elapsed_time))
+        self.label.configure(text=now)
+        self.root.after(1000, self.update_clock)
+
+app=App()
