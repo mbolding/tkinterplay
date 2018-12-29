bee5504e79ae60a41739977594671eb21aa916a6
Mark Bolding <mark.bolding@gmail.com>
2018-12-29 02:38:54 -0600

    more Euler

--- Euler2.py
+++ Euler2.py
@@ -17,3 +17,49 @@ n = next(F2)
 while n < 100:
     n = next(F2)
     print(n)
+
+
+def fibGen(n):
+    a, b = 0, 1
+    while n > 0:
+        yield a
+        a, b, n = b, a + b, n - 1
+
+
+[i for i in fibGen(11)]
+
+
+def fibGen2(n):
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a + b
+
+
+[k for k in fibGen2(100)]
+
+[k for k in fibGen2(1000)]
+
+[k for k in fibGen2(10000)]
+
+
+def fibGen3(n):
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a + b
+        a, b = b, a + b
+        a, b = b, a + b
+
+
+[k for k in fibGen3(1000)]
+
+[k for k in fibGen3(10000)]
+
+[k for k in fibGen3(400000)]
+
+[k for k in fibGen3(4e5)]
+
+[k for k in fibGen3(4e6)]
+
+sum([k for k in fibGen3(4e6)])
--- Euler4.py
+++ Euler4.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python
+def palindrome(start, stop):
+    a = []
+    for i in range(start, stop):
+        s = str(i)
+        a.append(int(s + s[::-1]))
 
-palindrome = 1001  # set starting number
-B = 10  # B = base used, default is base 10
+    return a[::-1]
 
-k = 0  # k = number of digits of 'palindrome'
-m = palindrome
-while m != 0:  # find k
-    m //= B
-    k += 1
 
-while k <= 4:
+P = palindrome(100, 1000)
 
-    print(palindrome)
 
-    m = (k - 1) // 2  # compute the middle position
-    l = m  # get at the middle position
+def divisable(n, start, stop):
+    for i in range(stop, start, -1):
+        if n % i == 0:
+            return i
 
-    while l >= 0:  # l < 0 marks the end of the current k
 
-        # get the digit at position l (counting from l=0)
-        digit = (palindrome // (B ** l)) % B
+divisable(9009, 10, 100)
 
-        if digit == B - 1:  # check if digit at l is the last digit 'B-1'
-            l -= 1  # if true then base case for l is reached
-            continue  # so shift right, to a lower l position
 
-        add = B ** l
-        if l < k // 2:  # will evaluate to false only when k%2=1 and l=m
-            add += B ** (l + 1)
-
-        palindrome += add
-        print(palindrome)
-
-        l = m  # get at the middle position
-
-    palindrome += 2  # base-case for changing the number of digits
-    k += 1
+for p in P:
+    d = divisable(p, 100, 1000)
+    if d:
+        q = p / d
+        if q < 1000:
+            print(p, d, q)
+            break
