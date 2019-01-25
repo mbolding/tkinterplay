7af370d94f0e110176b0dd43b9bbd76bc5ac40d1
Mark Bolding <mark.bolding@gmail.com>
2019-01-24 21:09:15 -0600

    e22

--- e/e22.py
+++ e/e22.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+
 file = open("e/p022_names.txt")
-lines = file.readlines()
-lines.sort()
+lines = sorted(file.read().split(","))
+
+
+file.close()
+lines[0].replace('"', "")
+[ord(c) - 64 for c in lines[0] if c != '"']
+
+for i in range(11):
+    print(lines[i], sum([ord(c) - 64 for c in lines[i] if c != '"']))
+
+idx = 0
+for line in lines:
+    idx += 1
+    if line == '"COLIN"':
+        break
+idx
+sum([ord(c) - 64 for c in '"COLIN"' if c != '"'])
+
+idx = 0
+total = 0
+for line in lines:
+    idx += 1
+    total += idx * sum([ord(c) - 64 for c in line.replace('"', "")])
+
+total
+idx
